d9836b7dc5576970b5caa06f8d5bbc3723eeb3e8
AdamChen <AdamChen@altek.com.tw>
2025-01-15 10:36:10 +0800

    add notes app

---
+++ NoteApp/NoteApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace NoteApp
+{
+    public partial class MainPage : ContentPage
+    {
+        string _fileName = Path.Combine(FileSystem.AppDataDirectory, "note.txt");
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // check file ecistence
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        async void OnSave(object sender, EventArgs e)
+        {
+            await DisplayAlert("Info", "Save button is clicked!!!", "Ok");
+        }
+
+        async void OnCancel(object sender, EventArgs e)
+        {
+            await DisplayAlert("Info", "Cancel button is clicked!!!", "Ok");
+        }
+    }
+
+}
