ca54604dce9f33e482745a4488838504a9b91756
AdamChen <AdamChen@altek.com.tw>
2025-01-16 10:51:24 +0800

    add read and exit button

--- NoteApp/NoteApp/MainPage.xaml.cs
+++ NoteApp/NoteApp/MainPage.xaml.cs
@@ -17,12 +17,33 @@
 
         async void OnSave(object sender, EventArgs e)
         {
+            System.Diagnostics.Trace.WriteLine("+++ OnSave() +++");
             await DisplayAlert("Info", "Save button is clicked!!!", "Ok");
+            File.WriteAllText(_fileName, editor.Text);
+            System.Diagnostics.Trace.WriteLine("xxx OnSave() xxx");
         }
 
         async void OnCancel(object sender, EventArgs e)
         {
             await DisplayAlert("Info", "Cancel button is clicked!!!", "Ok");
+            // clear text in screen
+            editor.Text = "";
+
+        }
+
+        private void OnRead(object sender, EventArgs e)
+        {
+            // read text from note.txt
+            editor.Text = File.ReadAllText(_fileName);
+        }
+
+        private async void onExit(object sender, EventArgs e)
+        {
+            bool shouldExit = await DisplayAlert("Confirm", "Are you sure you want to exit?", "Yes", "No");
+            if (shouldExit)
+            {
+                Application.Current?.Quit();
+            }
         }
     }
 
