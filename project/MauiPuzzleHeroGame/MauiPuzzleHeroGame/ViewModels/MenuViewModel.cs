47c08047c33dd159a7827567f333b37341c1e499
AdamChen <powq2011@gmail.com>
2025-10-23 11:06:19 +0800

    [MauiPuzzleHeroGame]: add exit dialog

--- project/MauiPuzzleHeroGame/MauiPuzzleHeroGame/ViewModels/MenuViewModel.cs
+++ project/MauiPuzzleHeroGame/MauiPuzzleHeroGame/ViewModels/MenuViewModel.cs
@@ -44,14 +44,23 @@ namespace MauiPuzzleHeroGame.ViewModels
         private async Task ExitAsync()
         {
             Util.Log("Exiting application...");
+            // Show alert to tell user the app is exiting
+            await Shell.Current.DisplayAlert("離開", "此遊戲即將離開?", "確定").ContinueWith(
+                 action =>
+                 {
 #if ANDROID
-            // Android exit application
-            Android.OS.Process.KillProcess(Android.OS.Process.MyPid());
+                     // Android exit application
+                     Android.OS.Process.KillProcess(Android.OS.Process.MyPid());
 #elif WINDOWS
-            Environment.Exit(0);
-#else
-        await Shell.Current.DisplayAlert("Alert", "Not support in the current platform!!!", "OK");
+                     Environment.Exit(0);
+#elif IOS
+                    // iOS exit application
+                    // Note: Apple discourages programmatic exits, this is just for demonstration.
+                    UIKit.UIApplication.SharedApplication.PerformSelector(new ObjCRuntime.Selector("terminateWithSuccess"), null, 0);
 #endif
+                 }
+                );
+
         }
 
 
